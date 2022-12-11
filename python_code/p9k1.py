e6e5f0a61686812db398c4f7a2ec574a3f9ad63c
AdonNeet <baradontap@gmail.com>
2022-12-11 23:03:37 +0700

    Try to commit from vscodium, this is was writen in COMMIT_EDITMSG

--- python_code/p9k1.py
+++ python_code/p9k1.py
@@ -1,5 +1,6 @@
 # Author menggunakan windows
 # C:/Users/[User Name]/AppData/Local/Programs/Python/Python311
+
 berkas = open("L200220194", "w")
 berkas.write('L200220194\n')
 berkas.write('10-02-2005\n')
