c4d0f0a877c41071932a3ac680377e07999cc878
AdonNeet <baradontap@gmail.com>
2022-12-19 07:08:55 +0700

    Update the code

---
+++ praktikum10/client2.py
@@ -0,0 +1,20 @@
+import socket
+hostname = "localhost"
+msg = ""
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((hostname, 50004))
+
+while(msg.lower() != "exit"):
+    msg = input("Command : ")
+    s.send(bytes(msg, "utf-8"))
+    if(msg.lower() == "exit"):
+        response = s.recv(1024)
+        temp = response.decode("utf-8")
+        print("Answer :", temp)
+        s.close()
+        break
+    elif(msg.lower() != "exit"):
+        response = s.recv(1024)
+        temp = response.decode("utf-8")
+        print("Answer :", temp)
+s.close()
---
+++ praktikum10/server2.py
@@ -0,0 +1,43 @@
+import socket
+import platform
+
+hostname = "localhost"
+s =socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((hostname, 50004))
+s.listen(5)
+print("Communication Program About My Platform")
+data = ""
+tmp = ""
+
+react = {
+        "machine" : platform.machine(),
+        "release" : platform.release(),
+        "system" : platform.system(),
+        "version" : platform.version(),
+        "node" : platform.node(),
+        "exit" : 'Ok!'
+        }
+
+
+while(tmp.lower()!='exit'):
+    clientsocket, address = s.accept()
+    while(tmp.lower()!='exit'):
+        data = clientsocket.recv(1024)
+        tmp = data.decode("utf-8")
+        # bagian menampilkan data yang diminta
+        print("Command :", tmp)
+
+        # bagian sistem response server
+        if(tmp.lower() in react):
+            msg = react[tmp]
+            clientsocket.send(bytes(msg, "utf-8"))
+        else:
+            msg = "Sorry, the command is unknown"
+            clientsocket.send(bytes(msg, "utf-8"))
+
+        # bagian sistem mematikan diri
+        if(tmp.lower()=='exit'):
+            print('Mematikan server')
+            s.close()
+            break
+        
