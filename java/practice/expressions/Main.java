4ce6f4d46f6a0828b5962c5b23e2731a21c04730
AdonNeet <baradontap@gmail.com>
2023-01-29 19:42:23 +0700

    update

---
+++ java/practice/expressions/Main.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String[] args){
+        // expression = operands and operators
+        // operands = values, variables, numbers, quantity
+        // operators = + - * / %
+
+        // yeah, thats is the basic
+
+        
+    }
+}
