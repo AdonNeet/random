88053e1a92dd0599ef9b0478b206cf8c54d6f89e
AdonNeet <baradontap@gmail.com>
2023-01-29 20:49:52 +0700

    update

--- java/practice/03-expressions/Main.java
+++ java/practice/03-expressions/Main.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+import java.lang.Math; // Java Math Class
+
 public class Main{
     public static void main(String[] args){
         // expression = operands and operators
@@ -6,8 +9,15 @@ public class Main{
 
         // yeah, thats is the basic
 
-        int a, b, c;
+        Scanner input = new Scanner(System.in);
 
+        int j, t;
+        System.out.println("Program menghitung ukuran tabung");
+        System.out.print("Masukkan nilai jari2: "); j = input.nextInt();    // its linear input mode
+        System.out.print("Masukkan nilai tinggi: "); t = input.nextInt();
+
+        Double ans = (Double) Math.PI * Math.pow(j, 2) * t;
         
+        System.out.println("Hasilnya adalah "+ans);
     }
 }
