3e739975ec132ac52f3c6207b7669f0c8e88ff38
AdonNeet <baradontap@gmail.com>
2023-01-29 21:04:29 +0700

    update

---
+++ java/practice/04-guiBasic/Main.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+
+public class Main {
+    public static void main(String[] args){
+        
+        String name = JOptionPane.showInputDialog("Masukkan nama kamu");
+        JOptionPane.showMessageDialog(null, "Hello "+name);
+
+        // for take a int or double input must use parse for each input...
+        int age =  Integer.parseInt(JOptionPane.showInputDialog("Masukkan umur kamu"));
+        double height = Double.parseDouble(JOptionPane.showInputDialog("Masukkan tinggi kamu"));
+        JOptionPane.showMessageDialog(null, "Umur kamu "+age+"\nTinggi kamu "+height);
+    }
+}
