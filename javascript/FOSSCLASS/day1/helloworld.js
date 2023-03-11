887db4eb5fd20a57c94a5ecb71639ec0ffeea250
AdonNeet <baradontap@gmail.com>
2023-03-12 04:16:44 +0700

    update

---
+++ javascript/FOSSCLASS/day1/helloworld.js
@@ -0,0 +1,21 @@
+// ini komentar
+
+/*
+    Ini 
+    Komentar
+    Multi-Line
+*/
+
+console.log("Hello World!");
+
+let x = 10;
+console.log(2*x+4);
+
+x *= 10;
+console.log(x);
+
+x -= 80;
+console.log(x);
+
+x /= 2;
+console.log(x);
