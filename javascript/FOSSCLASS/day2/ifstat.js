d8f0787ec9525e820656f843e56c0fd7b7be692d
AdonNeet <baradontap@gmail.com>
2023-04-08 16:30:47 +0700

    update

---
+++ javascript/FOSSCLASS/day2/ifstat.js
@@ -0,0 +1,11 @@
+const nilai = 80;
+
+if(nilai>=80){
+    console.log('A');
+}else if(nilai >=70){
+    console.log('B');
+}else if(nilai >= 50){
+    console.log('C');
+}else{
+    console.log('D');
+}
---
+++ javascript/FOSSCLASS/day2/looping.js
@@ -0,0 +1,21 @@
+let x = 0;
+
+// while looping
+while(true){
+    console.log(x);
+    x++;
+    if(x>2){
+        break;
+    }
+}
+
+// do looping
+do{
+    console.log('Hello World');
+    x--;
+} while(x>0);
+
+// for looping
+for (let i = 0; i < 3; i++) {
+    console.log(i);
+  }
