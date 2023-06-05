a78aee8b928c151d883d77ff4d33427c6040740e
AdonNeet <baradontap@gmail.com>
2023-06-05 17:06:45 +0700

    minor update

--- javascript/FOSSCLASS/day3/db_work.js
+++ javascript/FOSSCLASS/day3/db_work.js
@@ -1,7 +1,14 @@
+/*  
+    Author : adon_neet
+    Github : https://github.com/AdonNeet
+    Date   : M6/D5/Y2023
+
+*/
+
 const order = [];
 const database = [];
 
-// var to string
+// var to string, to get the name of variable
 const varToStr = varObj => Object.keys(varObj)[0];
 
 // add function
@@ -12,30 +19,37 @@ function add(data) {
 
 // get function
 function get(data) {
-    for (obj in order) {
-        if (data === obj) {
-            // get index
+    // get the index of object
+    const index = order.indexOf(data);
+    // get all data, i think the output promises such as a table
 
-            // get all data, i think the output want to be like in squirel
-
-        }
-    }
 }
 
 // update function
-function update(/* what dou you want update? */) {
-    ;
+function update(property=null, values=null) {
+    // update property (known as data), i think it must can update many property in there :D
+    if(property != null && values != null) {
+        ;
+    }
 }
 
 // delete function, to delete user in database
 function remove(data) {
-    ;
+    // get the index of object
+    const index = order.indexOf(data);
+    // remove object with splice()
+    if (index > -1) {
+        order.splice(index, 1);
+        database.splice(index, 1);
+    }
 }
 
 
 
 
 
+
+// there is data that will inputed in database
 
 const user1 = {
     id: "1",
