9c5813c1a264d344987deea3f0be0d92ea6996c3
AdonNeet <baradontap@gmail.com>
2023-06-06 11:52:27 +0700

    minor update

--- javascript/FOSSCLASS/day3/coretan.js
+++ javascript/FOSSCLASS/day3/coretan.js
@@ -1,8 +1,9 @@
 const database = []
 
 // the default coloumn in this database
-const ALL = ['id', 'name', 'age', 'gender', 'address'];
 const [id, name, age, gender, address] = ['id', 'name', 'age', 'gender', 'address'];
+const ALL = ['id', 'name', 'age', 'gender', 'address'];
+
 
 
 
@@ -10,11 +11,11 @@ const [id, name, age, gender, address] = ['id', 'name', 'age', 'gender', 'addres
 const varToStr = varObj => Object.keys(varObj)[0];
 
 function findIndex(prop, value) { // only one condition
-    var index = database.map(function(e) {return e[prop]; }).indexOf(value);
+    let index = database.map(function(e) {return e[prop]; }).indexOf(value);
     return index;
 }
 
-// add funct done
+// add funct done, yey
 function add(data) {
     if (Array.isArray(data) === true) {
         for (let i = 0;i<data.length;i++){
@@ -25,7 +26,52 @@ function add(data) {
     }
 }
 
-// get funct
+// remove funct (undone), index finder goes wrong for the remove and get funct
+function remove(condition){ //only for one user (?)
+    // find the index of object with condition
+    var index = [];
+    if (Array.isArray(condition) === true) {
+        for (let i = 0;i<database.length;i++) {
+            const trueCount = condition.length;
+            var found = false;
+            for(let j = 0;j<condition.length;j++){
+                var count = 0;
+                if(database[i][varToStr(condition[j])] === condition[j][varToStr(condition[j])]) {
+                    count++;
+                    if(count === trueCount) {   // something shit happening in there, didnt detect when its array
+                        found = true;
+                        break;
+                    } else {
+                        continue;   
+                    }
+                } else {
+                    break;
+                }
+            }
+            if (found === true) {
+                index.push(i);
+            }
+        }
+    } else {
+        for (let i = 0;i<database.length;i++) { // same like in that, can delete in specific object
+            if(database[i][varToStr(condition)] === condition[varToStr(condition)]) {
+                index.push(i);
+            }
+        }
+    }
+    console.log(index);
+    if (index.length > 1) {
+        var count = 0;
+        for (let idx = 0;idx<index.length;idx++){
+            database.splice(idx-count, 1);
+            count = count + 1;
+        }
+    }
+    
+}
+
+
+// get funct, undone, sike
 function get(subdata, condition){   // only for one user (?)
     // find the index of obj with condition, done
     var index = [];
@@ -62,7 +108,7 @@ function get(subdata, condition){   // only for one user (?)
             }
         }
     }
-    // the kind of subdata that want to get-ed
+    // the kind of subdata that want to get-ed, the output promises like a table, i think
     if (Array.isArray(subdata) === true) {
         if (varToStr(subdata) === 'ALL') {
          
@@ -77,15 +123,15 @@ function get(subdata, condition){   // only for one user (?)
 
 
 // user data that will inputed
-const user1 = {
+const user1 = {     // 0
     id: "1",
     name: "Adon",
-    age: "18",
+    age: "21",
     gender: "Male",
     address: "World",
 }
 
-const user2 = {
+const user2 = {     // 1
     id: "2",
     name: "Neet",
     age: "21",
@@ -93,7 +139,7 @@ const user2 = {
     address: "Isekai",
 }
 
-const user3 = {
+const user3 = {     // 2
     id: "3",
     name: "Kari",
     age: "23",
@@ -101,15 +147,35 @@ const user3 = {
     address: "Isekai",
 }
 
+const user4 = {     // 3
+    id: "4",
+    name: "Weebs",
+    age: "21",
+    gender: "Male",
+    address: "Isekai",
+}
+
+const user5 = {     // 4
+    id: "5",
+    name: "Basi",
+    age: "21",
+    gender: "Female",
+    address: "Isekai",
+}
+
 // add(user1); add(user2); add(user3);
-add([user1, user2, user3]);
+add([user1, user2, user3, user4, user5]);
+add({id: "6", name: "Ran", age: "25", gender: "Male", address: "World"})
 
 console.log(database);
 
 for (let i = 0;i<database.length;i++) {
-    console.log(Object.values(database[i]));
+    console.log(database[i]);
 }
 
 var idx = database.map(function(e) { return e['name']; }).indexOf('Kari');
 console.log(idx);
 console.log(findIndex('name', 'Kari'));
+
+remove({age: "21", gender: "Male"});
+console.log(database)
